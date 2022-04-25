25ae60f767bc10fa48540cbd8f31aa6548e337d6
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-25 03:07:21 +0100

    created a second end point named get

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.post("/create", (req, res) => {
   );
 });
 
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Running on port 8080`);
 });
